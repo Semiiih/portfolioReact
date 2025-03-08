bd7749b8223bbc5c6fe39b0f932678179f1930cc
Semih Cakici <semih.cakici@snowpact.com>
2025-03-08 12:51:51 +0100

    add competence

--- src/components/atoms/CompetenceSlideBlock/CompetenceSlideBlock.tsx
+++ src/components/atoms/CompetenceSlideBlock/CompetenceSlideBlock.tsx
@@ -8,23 +8,35 @@ import logoCSS from "@/assets/css.png";
 import logoYarn from "@/assets/yarn.png";
 import logoStorybook from "@/assets/storybook.png";
 import FancyText from "@carefully-coded/react-text-gradient";
+import logoPython from "@/assets/logoPython.webp";
+import logoSQLite from "@/assets/SQLite.webp";
+import logoDjangoNoir from "@/assets/DjangoNoir.webp";
+import php from "@/assets/PHP.svg";
+import typeScript from "@/assets/typeScript.svg";
+import java from "@/assets/java.png";
 
 const competences = [
   { src: logoNext, name: "Next.js" },
   { src: logoReact, name: "React" },
   { src: logoJS, name: "JavaScript" },
   { src: logoTailwind, name: "Tailwind CSS" },
+  { src: typeScript, name: "TypeScript" },
+  { src: java, name: "Java" },
   { src: logoNpm, name: "npm" },
   { src: logoHTML, name: "HTML" },
   { src: logoCSS, name: "CSS" },
+  { src: php, name: "PHP" },
+  { src: logoPython, name: "Python" },
   { src: logoYarn, name: "Yarn" },
   { src: logoStorybook, name: "Storybook" },
+  { src: logoSQLite, name: "SQLite" },
+  { src: logoDjangoNoir, name: "Django" },
 ];
 
 export const CompetenceSlideBlock = () => {
   return (
     <div className="relative m-auto w-full overflow-hidden  bg-transparent py-20 before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] after:content-[''] lg:w-2/3">
-      <div className="flex w-[calc(250px*10)] animate-infinite-slider">
+      <div className="flex w-[calc(350px*10)] animate-infinite-slider">
         {competences.map((logo, index) => (
           <div
             className="slide group relative flex w-[125px] flex-col items-center justify-center"
